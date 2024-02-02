94a44e336dbdb364f3d415b4153d97a655ac105f
antonpihlajaniemi <c3pian00@students.oamk.fi>
2024-02-02 11:00:30 +0200

    arvaa numero  lisätty

---
+++ viikkotehtävä 4/arvaa numero/js/functions.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+
+const getRandomIntNumberInRange = (min,max) =>  {
+    return Math.floor(Math.random() * max) + min
+}
+
+
+button.addEventListener('click', ()=> {
+    const randomizeNumber = getRandomIntNumberInRange(1,6)
+    const guess =  Number(input.value)
+
+    if (guess === randomizeNumber) {
+        alert("Arvasit oiken, numero on " + randomizeNumber)
+    } else {
+        alert("Arvasit väärin, numero on " + randomizeNumber)
+    }
+    input.value=''
+    input.focus()
+})
