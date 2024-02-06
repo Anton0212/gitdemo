12236ff857b164f3bb280ea7b6916964477221f1
antonpihlajaniemi <c3pian00@students.oamk.fi>
2024-02-06 23:11:03 +0200

    noppa

--- viikkotehtävä 4/noppa/js/functions.js
+++ viikkotehtävä 4/noppa/js/functions.js
@@ -1,14 +1,30 @@
 
-var dice = document.getElementById("dice");
+function rollDice() {
+    var randomNumber = Math.floor(Math.random() * 6) + 1; 
+    var diceImage = document.getElementById('diceImage');
 
 
-dice.addEventListener("click", function() {
-   
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
-    
-    
-    var diceImage = document.getElementById("diceImage");
-    
-   
-    diceImage.src = "" + randomNumber + "dice.five.png"; 
-});
+    switch (randomNumber) {
+        case 1:
+            diceImage.src = 'img/dice-one.png';
+            break;
+        case 2:
+            diceImage.src = 'img/dice-two.png';
+            break;
+        case 3:
+            diceImage.src = 'img/dice-three.png';
+            break;
+        case 4:
+            diceImage.src = 'img/dice-four.png';
+            break;
+        case 5:
+            diceImage.src = 'img/dice-five.png';
+            break;
+        case 6:
+            diceImage.src = 'img/dice-six.png';
+            break;
+        default:
+            break;
+    }
+}
+
